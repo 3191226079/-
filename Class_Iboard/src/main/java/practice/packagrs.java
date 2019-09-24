9aaf728b12789f1f4118f5ba8368907e04bbefc9
3191226079@qq.com <3191226079@qq.com>
2019-09-24 10:01:57 +0800

    第二次

---
+++ Class_Iboard/src/main/java/practice/packagrs.java
@@ -0,0 +1,12 @@
+package practice;
+
+import java.util.ArrayList;
+
+public class packagrs {
+	public static void main(String[] args) {
+		String trues = null;
+		String e = trues;
+		ArrayList<String> list = new ArrayList<String>();
+		list.add(e);
+	}
+}
